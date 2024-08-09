2fc45c1cd6bdbd489c25042a90f12988e449fc01
unknown <ckeziah95@gmail.com>
2024-08-09 15:38:35 +0000

    added to retry the responsiveness

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <header className="bg-white shadow-md p-4 flex justify-between items-center">
             <h1 className="text-2xl font-bold text-center">News-Demo</h1>
             <nav>
-                <ul className="flex space-x-4">
+                <ul className="flex space-x-4 overflow-x-auto text-sm border-t">
                     <li><a href="/" className="text-gray-700 hover:text-blue-500">Home</a></li>
 
                     <li><a href="/foryou" className="text-gray-700 hover:text-blue-500">For You</a></li>
